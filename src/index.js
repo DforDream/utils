bb7fbb139de182292175f56377c9606e24cec9fb
dxp <1500549325@qq.com>
2023-11-22 19:32:59 +0800

    13

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import { Stack } from './Stack'
-import name from './test/index.js'
-export {
-  Stack
-}
 
 export default {
-  a: '123',
-  name
-}
-
-module.exports = {
-  name,
-  a: '123'
+  Stack
 }
