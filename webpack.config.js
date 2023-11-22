64dd22e28af3cf0d8f43d165323514293f007472
dxp <1500549325@qq.com>
2023-11-22 19:09:46 +0800

    9

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "./src/index.js"),
+  output: {
+    path: path.join(__dirname, "lib/"),
+    // filename: "[name].[hash:6].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+};
